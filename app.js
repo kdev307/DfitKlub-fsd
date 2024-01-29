dce2e61201bb177bca2a19537a1bc593c3164224
kdev307 <kumardev30072002@gmail.com>
2024-01-29 20:47:04 +0530

    Project Creation & DB Connect

---
+++ app.js
@@ -0,0 +1,84 @@
+// Required Modules
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+// View Engine Setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+// Connecting to the Database
+const mysql = require("mysql2");
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Abcd1234!@#$",
+    database: "dfitklub-fsd",
+});
+conn.connect();
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+app.get("/", function (req, res, next) {
+    res.render("index");
+});
+
+app.get("/index", function (req, res, next) {
+    res.render("index");
+});
+
+app.get("/store", function (req, res, next) {
+    res.render("store");
+});
+
+app.get("/signIn", function (req, res, next) {
+    res.render("signIn");
+});
+
+app.get("/signUp", function (req, res, next) {
+    res.render("signUp");
+});
+
+app.get("/adminSignIn", function (req, res, next) {
+    res.render("adminSignIn");
+});
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+// Catch 404 and Forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// Error Handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
---
+++ public/javascripts/storeFilter.js
@@ -0,0 +1,39 @@
+const filterContainer = document.querySelector(".product-filter");
+const productItems = document.querySelectorAll(".product-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) {
+        // Remove 'active' class from the previously active filter
+        filterContainer.querySelector(".active").classList.remove("active");
+
+        // Add 'active' class to the clicked filter
+        event.target.classList.add("active");
+
+        // Get the filter value from the data-filter attribute
+        const filterValue = event.target.getAttribute("data-filter");
+
+        // Iterate over each product item
+        productItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                // Show the 'filter-all' element when filter is not 'all'
+                if (filterValue === "sports")
+                    document.getElementById("sport-cat").style.display = "flex";
+                if (filterValue !== "all") {
+                    document.getElementById("filter-all").style.display = "block";
+                    document.getElementById("filter-all").style.fontSize = "1.2rem";
+                } else {
+                    // Hide the 'filter-all' element when filter is 'all'
+                    document.getElementById("filter-all").style.display = "none";
+                    document.getElementById("sport-cat").style.display = "none";
+                }
+                // Remove 'hide' and add 'show' classes
+                item.classList.remove("hide");
+                item.classList.add("show");
+            } else {
+                // Remove 'show' and add 'hide' classes
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
